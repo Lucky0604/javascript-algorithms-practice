5a37f7e5196db6a81ac4e265a7ed0341e966dffc
Lucky <175917645@qq.com>
2016-06-25 15:14:39 +0800

    basic hash table's method

--- test/datastructure/linked_list_test.js
+++ test/datastructure/linked_list_test.js
@@ -1,4 +1,4 @@
-var LL = require('../../datastructure/linked-list.js')
+var LL = require('../../datastructure/linked-list.js');
 var linkedList = new LL.LinkedList();
 linkedList.push({
     name: 'John',
@@ -12,5 +12,5 @@ linkedList.push({
     name: 'Garry',
     birthYear: 1989
 });
-console.log(linkedList.shift().data)
-console.log(linkedList.pop().data)
+console.log(linkedList.shift().data);
+console.log(linkedList.pop().data);
