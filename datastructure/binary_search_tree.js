24eefdf7a0a6522ffb67f28bce19db0920071bc1
Lucky <175917645@qq.com>
2016-06-26 23:24:02 +0800

    add findMin and findMax of the whole tree

--- datastructure/binary_search_tree.js
+++ datastructure/binary_search_tree.js
@@ -187,6 +187,125 @@
         }
     };
 
+    
+    /**
+     * replace given child with new one. for given parent.
+     *
+     * @private
+     * @param {Node} parent Parent node.
+     * @param {Node} oldChild Child to be replaced
+     * @param {Node} newChild Child replacement
+     */
+    exports.BinaryTree.prototype._replaceChild = function(parent, oldChild, newChild) {
+        if (!parent) {
+            this._root = newChild;
+            if (this._root !== null) {
+                this._root._parent = null;
+            }
+        } else {
+            if (parent._left === oldChild) {
+                parent._left = newChild;
+            } else {
+                parent._right = newChild;
+            }
+            if (newChild) {
+                newChild._parent = parent;
+            }
+        }
+    };
 
+
+    /**
+     * Removes node from the tree.
+     * Average runtime complexity: O(log N)
+     *
+     * @public
+     * @param {Node} node to be removed
+     * @returns {Boolean} True/false depending on whether the given node is removed.
+     */
+    exports.BinaryTree.prototype.remove = function(node) {
+        if (!node) {
+            return false;
+        }
+        if (node._left && node._right) {
+            var min = this._findMin(node._right);
+            var temp = node.value;
+            node.value = min.value;
+            min.value = temp;
+            return this.remove(min);
+        } else {
+            if (node._left) {
+                this._replaceChild(node._parent, node, node._left);
+            } else if (node._right) {
+                this._replaceChild(node._parent, node, node._right);
+            } else {
+                this._replaceChild(node._parent, node, null);
+            }
+            return true;
+        }
+    };
+
+    
+    /**
+     * Finds the node with minium value in given sub-tree
+     * 
+     * @private
+     * @param {Node} node Root of the sub-tree
+     * @param {Number | String} current Current minium value of the sub-tree
+     * @return {Node} Node with the minium value in the sub-tree
+     */
+    exports.BinaryTree.prototype._findMin = function(node, current) {
+
+        current = current || {value: Infinity};
+        if (!node) {
+            return current;
+        }
+        if (current.value > node.value) {
+            current = node;
+        }
+        return this._findMin(node._left, current);
+    };
+
+
+    /**
+     * Finds the node with maximum value in the given sub-tree
+     *
+     * @private
+     * @param {Node} node Root of the sub-tree
+     * @param {Number|String} current Current maximum value of the sub-tree
+     * @returns {Node} Node with the maximum value in the sub-tree
+     */
+    exports.BinaryTree.prototype._findMax = function(node, current) {
+
+        current = current || {value: -Infinity};
+        if (!node) {
+            return current;
+        }
+        if (current.value < node.value) {
+            current = node;
+        }
+        return this._findMax(node._right, current);
+    };
+
+
+    /**
+     * Finds the node with minium value in the whole tree
+     *
+     * @public
+     * @returns {Node} The minium node of the tree.
+     */
+    exports.BinaryTree.prototype.findMin = function() {
+        return this._findMin(this._root);
+    };
+
+    /**
+     * Finds the node with maximum value in the whole tree.
+     *
+     * @public
+     * @returns {Node} The maximum node of the tree
+     */
+    exports.BinaryTree.prototype.findMax = function() {
+        return this._findMax(this._root);
+    };
 
 })
